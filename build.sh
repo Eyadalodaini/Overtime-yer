0dcfe53ad2736ed72b711e9bd8607fcb0ec3730a
Eyadalodaini <eaadalodaini@gmail.com>
2026-09-06 21:07:02 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-flutter create . --platforms=android
-flutter pub get
-flutter analyze
-flutter build apk --release
-echo "APK: build/app/outputs/flutter-apk/app-release.apk"
